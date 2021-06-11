e45ecfdbbb8c4598e6f75d3055d7e3af6b546b12
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-06-11 17:41:47 -0500

    add sms notification in case of exceeding or preceding threshold

--- src/monitor.py
+++ src/monitor.py
@@ -1,12 +1,20 @@
 from concurrent.futures import ThreadPoolExecutor
+from json import load
+from logging import INFO, basicConfig, getLogger
+from os import listdir
 from pprint import pprint
 from re import findall
+from smtplib import SMTP, SMTPException
 
 from bs4 import BeautifulSoup
 from requests import get
 
+basicConfig(level=INFO, datefmt='%b-%d-%Y %H:%M:%S',
+            format='%(asctime)s - %(levelname)s - %(funcName)s - Line: %(lineno)d - %(message)s')
+logger = getLogger('monitor.py')
 
-def analyze(ticker, max_threshold: float, min_threshold: float):
+
+def analyze(ticker: str, max_threshold: float, min_threshold: float):
     """Analyzes the ticker value from finance.yahoo.com and compares with the max and min price value.
 
     Args:
@@ -27,18 +35,48 @@ def analyze(ticker, max_threshold: float, min_threshold: float):
         result = 'decreased'
     elif 'positiveColor' in str(raw_data):
         result = 'increased'
-    msg = f"The current price of {ticker} is: ${price}\n{ticker} share has {result} ${price_change[-2]}"
+    logger.info(f"The current price of {ticker} is: ${price}\n{ticker} share has {result} ${price_change[-2]}")
     message = None
     if price < min_threshold:
-        message = f'{ticker} is currently less than ${min_threshold}.\n\n{msg}'
+        message = f'{ticker} is currently less than ${min_threshold}.'
     elif price > max_threshold:
-        message = f'{ticker} is currently more than ${max_threshold}.\n\n{msg}'
+        message = f'{ticker} is currently more than ${max_threshold}.'
     if message:
-        print(message)
-        print(f'https://finance.yahoo.com/quote/{ticker}')
+        logger.info(message)
+        notify(subject=ticker, body=message)
 
 
-def get_all_cryptos(offset):
+def notify(subject: str, body: str):
+    """Notification is triggered when the case status, is other than 'Case Was Received'.
+
+    Notifies via text message through SMS gateway of destination number.
+
+    Args:
+        body: Receives the content as argument `body` to send an SMS notification.
+        subject: Receives the title as argument `subject` to send an SMS notification.
+
+    """
+    try:
+        to = f"{phone_number}@tmomail.net"
+        message = (f"From: {email_sender}\n" + f"To: {to}\n" + f"Subject: {subject}\n" + "\n\n" + body)
+        server = SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(user=email_sender, password=email_password)
+        server.sendmail(email_sender, to, message)
+        server.quit()
+        server.close()
+        logger.info('SMS notification has been sent.')
+    except SMTPException as error:
+        if 'Username and Password not accepted' in str(error):
+            logger.error(error)
+            exit(1)
+        else:
+            logger.error('Failed to send SMS. Please check the GMAIL_USER and GMAIL_PASS in params.json.')
+            logger.critical('Logon to https://myaccount.google.com/lesssecureapps and turn ON less secure apps.')
+            exit(error)
+
+
+def get_all_cryptos(offset: int):
     """Update a global dictionary with all cryptocurrency ticker values, current price and ticker name.
 
     Args:
@@ -55,14 +93,35 @@ def get_all_cryptos(offset):
     for index, link in enumerate(name_list):
         title = link.get('title')
         ticker = link.get('href').split('?p=')[-1]
-        cryptos[num_list[index]] = [ticker, title]
+        cryptos[num_list[index]] = [ticker, title]  # updated dictionary with ticker, name and current price
 
 
 if __name__ == '__main__':
+    if 'params.json' not in listdir('src'):
+        exit('Script requires a json file (params.json) with credentials stored as key value pairs.')
+
+    if not open('src/params.json').read():
+        logger.error('Credentials file is empty.')
+        phone_number, email_sender, email_password = None, None, None
+    else:
+        json_file = load(open('src/params.json'))
+        phone_number = json_file.get('PHONE')
+        email_sender = json_file.get('GMAIL_USER')
+        email_password = json_file.get('GMAIL_PASS')
+
+    env_vars = [phone_number, email_sender, email_password]
+
+    if any(env_var is None for env_var in env_vars):
+        exit("Your 'params.json' should appear as following:\n"
+             "{\n"
+             "\tPHONE: <phone_number>,\n"
+             "\tGMAIL_USER: <sender_email_address>,\n"
+             "\tGMAIL_PASS: <sender_id_password>,\n"
+             "}")
+
     analyze(ticker='BTC-USD', max_threshold=37_000, min_threshold=30_000)
 
     cryptos = {}
     with ThreadPoolExecutor(max_workers=5) as executor:
         executor.map(get_all_cryptos, list(range(0, 500, 100)))
     pprint(cryptos)
-    print(len(cryptos))
